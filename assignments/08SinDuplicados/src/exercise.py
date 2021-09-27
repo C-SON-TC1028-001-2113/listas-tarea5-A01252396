a7f7c8c3c99b26de962a6475c21c33294d65b460
A01252396 <A01252396@ITESM.MX>
2021-09-27 05:08:58 +0000

    Tarea 5: Programas que utilizan listas

--- assignments/08SinDuplicados/src/exercise.py
+++ assignments/08SinDuplicados/src/exercise.py
@@ -1,8 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
-
-
-    pass
-
+    n=int(input())
+    Lista1=[]
+    Lista2=[]
+    if n>0: 
+        for c in range (n):
+            Elemento=input()
+            Lista1.append(Elemento)
+            if (Elemento in Lista2)==False:
+                Lista2.append(Elemento) 
+        print(Lista1)   
+        print(Lista2)       
+    else:
+        print('Error')  
 if __name__=='__main__':
     main()
--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,9 +1,12 @@
-
-
 def main():
-
-
-
-
+    r=int(input())
+    c=int(input())
+    m=r*c
+    Lista=[]
+    for n in range (m):
+        e=int(input())
+        if e<10:
+            Lista.append(e)
+    print(Lista)     
 if __name__=='__main__':
     main()
